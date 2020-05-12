ac9cf4c503c0992bc00b9c90a433527237d707b3
Julie <Julie@Julie-PC>
2020-05-12 15:16:29 +0100

    Moved GUITableobject back inside main method

--- src/databases/Databases.java
+++ src/databases/Databases.java
@@ -3,10 +3,8 @@ package databases;
 //import java.sql.*;
 public class Databases {
 
-    public static GUITable gt = new GUITable();
-
     public static void main(String[] args) {
-
+        GUITable gt = new GUITable();
         gt.setVisible(true);
 
 //        GUISingle gs = new GUISingle();
